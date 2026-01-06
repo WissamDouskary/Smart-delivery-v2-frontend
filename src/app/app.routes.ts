bebced66eb791657b30e48192d5dc6dec8c9b9ee
WissamDouskary <douskary.wissam@gmail.com>
2026-01-06 11:58:16 +0100

    feat: create routing with canActivate to check guards for securized paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,29 @@
 import { Routes } from '@angular/router';
-import { TestComponent } from './pages/test/test';
 import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { RoleGuard } from './Auth/RoleGuard';
+import { Unauthorized } from './pages/unauthorized/unauthorized';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
+import { AuthGuard } from './Auth/AuthGuard';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: Home,
-        pathMatch: 'full'
-    },
-    {
-        path: "test",
-        component: TestComponent
-    }
+  {
+    path: '',
+    component: Home,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboard,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { role: 'Manager' },
+  },
+  {
+    path: 'unauthorized',
+    component: Unauthorized,
+  },
 ];
