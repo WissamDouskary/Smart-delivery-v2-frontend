f49a6d200d15772cc75d9a3409ac7f323ccaae1a
WissamDouskary <douskary.wissam@gmail.com>
2026-01-06 12:12:49 +0100

    feat: implement a role and auth guard to use them in CanActivate

---
+++ src/app/Auth/RoleGuard.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth-service';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({ providedIn: 'root' })
+export class RoleGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    if (!isPlatformBrowser(this.platformId)) {
+      return true; 
+    }
+
+    const expectedRole = route.data['role'];
+    const userRole = this.authService.getUserRole();
+
+    if (!userRole || userRole !== expectedRole) {
+      this.router.navigate(['/unauthorized']);
+      return false;
+    }
+
+    return true;
+  }
+}
