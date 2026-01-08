596611170fb3c9d4cbedb849986ae65feea7dfc0
WissamDouskary <douskary.wissam@gmail.com>
2026-01-08 16:34:35 +0100

    fix(routes): update navigation routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './core/guards/auth.guard';
-import { RoleGuard } from './core/guards/role.guard';
 
 export const routes: Routes = [
   {
