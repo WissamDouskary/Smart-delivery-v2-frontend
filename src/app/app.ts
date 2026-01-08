ac1cbfac163e852d1354e92bc3553be59990e6b5
WissamDouskary <douskary.wissam@gmail.com>
2026-01-08 16:32:25 +0100

    feat(app): configure ngx-sonner in root component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar/navbar';
+import { NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgxSonnerToaster],
   templateUrl: "./app.html",
   styleUrl: './app.css'
 })
 
 export class App {
-  constructor(private router: Router) {}
-  
+  constructor(private router: Router) { }
+
   isAuthPage(): boolean {
     return this.router.url.startsWith('/auth');
   }
