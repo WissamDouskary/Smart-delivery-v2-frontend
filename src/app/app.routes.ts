8892d563a8993ac92f3656e4424978697b78efd2
WissamDouskary <douskary.wissam@gmail.com>
2026-01-08 10:02:05 +0100

    fix: remove loadChildren and simplify routes to resolve duplication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home.page';
-import { Login } from './features/auth/pages/login/login.page';
 import { Unauthorized } from './features/auth/pages/unauthorized/unauthorized';
 import { AuthGuard } from './core/guards/auth.guard';
 import { RoleGuard } from './core/guards/role.guard';
@@ -14,7 +13,11 @@ export const routes: Routes = [
   },
   {
     path: 'auth/login',
-    component: Login,
+    loadComponent: () => import('./features/auth/pages/login/login.page').then(m => m.Login),
+  },
+  {
+    path: 'auth/register',
+    loadComponent: () => import('./features/auth/pages/register/register.page').then(m => m.Register),
   },
   {
     path: 'unauthorized',
@@ -22,8 +25,7 @@ export const routes: Routes = [
   },
   {
     path: 'admin/dashboard',
-    loadComponent: () =>
-      import('./features/admin/dashboard/admin-dashboard').then(m => m.AdminDashboard),
+    loadComponent: () => import('./features/admin/dashboard/admin-dashboard').then(m => m.AdminDashboard),
     canActivate: [AuthGuard, RoleGuard],
     data: { role: 'Manager' },
   },
