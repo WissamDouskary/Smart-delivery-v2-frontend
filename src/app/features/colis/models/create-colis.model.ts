75a01840f8fc2db257db6e501ab9a46ee5f80f9f
WissamDouskary <douskary.wissam@gmail.com>
2026-01-17 18:29:05 +0100

    refactor: update CreateColisModel structure

--- src/app/features/colis/models/create-colis.model.ts
+++ src/app/features/colis/models/create-colis.model.ts
@@ -1,63 +1,43 @@
-import { COLIS_PRIORITY } from "./enums/colis-priority.enum";
-import { COLIS_STATUS } from "./enums/colis-status.enum";
+import { newSenderModel } from '../../../core/models/new-sender.model';
+import { COLIS_PRIORITY } from './enums/colis-priority.enum';
+import { COLIS_STATUS } from './enums/colis-status.enum';
 
 export interface createColisModel {
   description: string;
-  poids: number;
   vileDistination: string;
-  receiver: {
-    id: string;
-    nom: string;
-    prenom: string;
-    email: string;
-    telephone: string;
-    adresse: string;
-  } | {
-    id: string;
-  };
-  sender: {
-    id: string;
-    nom: string;
-    prenom: string;
-    email: string;
-    telephone: string;
-    adresse: string;
-  } | {
-    id: string;
-  };
-  livreur: {
-    id: string;
-    nom: string;
-    prenom: string;
-    email: string;
-    telephone: string;
-    adresse: string;
-  } | null | {
-    id: string;
-  };
+  receiver:
+    | {
+        id: string;
+      }
+    | {
+        nom: string;
+        prenom: string;
+        email: string;
+        telephone: string;
+        adresse: string;
+      };
+  sender:
+    | {
+        id: string;
+      }
+    | newSenderModel
+  products: [
+    (
+      | {
+          id: string;
+          quantity: number;
+        }
+      | {
+          nom: string;
+          category: string;
+          poids: number;
+          price: number;
+          quantity: number;
+        }
+    ),
+  ];
   city: {
     id: string;
-    nom: string;
-    codePostal: number;
-  } | {
-    id: string;
   };
-  historiqueLivraisonList: {
-    id: string;
-    status: string;
-    changementDate: string;
-    comment: string;
-  }[] | null;
-  colisProducts: {
-    id: string;
-    nom: string;
-    category: string;
-    poids: number;
-    price: number;
-    quantity: number;
-  }[] | {
-    id: string;
-  };
-  status: COLIS_STATUS;
-  priority: COLIS_PRIORITY;
+  priority: string;
 }
