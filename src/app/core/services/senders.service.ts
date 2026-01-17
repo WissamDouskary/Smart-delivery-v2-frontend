d143e333e7c3a3f44d89256ddef8a3a065c94fc9
WissamDouskary <douskary.wissam@gmail.com>
2026-01-17 18:28:42 +0100

    feat: add senders service

---
+++ src/app/core/services/senders.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { map, Observable } from 'rxjs';
+import { senderModel } from '../models/sender.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class senderService {
+  private apiSer = inject(ApiService);
+
+  getSenders(): Observable<senderModel[]> {
+    return this.apiSer.get<senderModel[]>(`sender`).pipe(
+      map((resp: any) => {
+        return resp.data
+      })
+    );
+  }
+}
